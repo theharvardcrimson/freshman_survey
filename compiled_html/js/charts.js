3da0c1077fd94916213e620ac70e5d3e7a8a3f09
Joseph Botros <jrbotros@gmail.com>
2013-09-06 01:47:25 -0400

    tweaks

--- compiled_html/js/charts.js
+++ compiled_html/js/charts.js
@@ -50,16 +50,16 @@ $(function () {
     createChart('bar','data/2/1/extracurriculars.csv', '#extracurriculars');
     createChart('bar','data/2/1/presidents_raw.csv', '#leadership');
     createChart('bar','data/2/1/government_belief.csv', '#government');
-    createMultiChart('column', ['Public', 'Private'], [colorset[colorindex++], colorset[colorindex++]], ['data/2/1/public_vs_math.csv', 'data/2/1/private_vs_math.csv'], '#math-school-type');
+    createMultiChart('column', ['Public', 'Private'], null, ['data/2/1/public_vs_math.csv', 'data/2/1/private_vs_math.csv'], '#math-school-type');
 
     // At Harvard
     createChart('pie','data/2/2/hours_studying_future.csv', '#study-habits-future');
     createChart('pie','data/2/2/secondary_raw.csv', '#secondary');
 
     // Athletics
-    createMultiChart('column', ['Walk-on', 'Recruited'], [colorset[colorindex++], colorset[colorindex++]], ['data/2/3/walkons_four_years.csv', 'data/2/3/recruited_four_years.csv'], '#attrition-athletics');
+    createMultiChart('column', ['Walk-on', 'Recruited'], null, ['data/2/3/walkons_four_years.csv', 'data/2/3/recruited_four_years.csv'], '#attrition-athletics');
     createChart('bar','data/2/3/concussions.csv', '#concussions');
-    createMultiChart('column', ['Never been concussed', 'Concussed Athletes'], [colorset[colorindex++], colorset[colorindex++]], ['data/2/3/not_concussed_changed_view.csv', 'data/2/3/concussed_changed_view.csv'], '#concussion-view');
+    createMultiChart('column', ['Never been concussed', 'Concussed Athletes'], null, ['data/2/3/not_concussed_changed_view.csv', 'data/2/3/concussed_changed_view.csv'], '#concussion-view');
 
 
 	$('.banner').each(function() {
